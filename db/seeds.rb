30b7a1cad331f36f5fcf4fd4913aac3f7fe091f5
Nicolas Agathos <nicolas.agathos@gmail.com>
2021-02-06 09:33:56 +0100

    newroom seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+the_newsroom = Company.create!(name: "The Newsroom")
+
+user_charlie = User.create!(
+  email: "charlie@newsroom.com",
+  password: "123123",
+  company: the_newsroom
+)
+
+user_will = User.create!(
+  email: "will@newsroom.com",
+  password: "123123",
+  company: the_newsroom
+)
+
+user_mac = User.create!(
+  email: "mac@newsroom.com",
+  password: "123123",
+  company: the_newsroom
+)
+
+user_sloan = User.create!(
+  email: "sloan@newsroom.com",
+  password: "123123",
+  company: the_newsroom
+)
+
+user_jim = User.create!(
+  email: "jim@newsroom.com",
+  password: "123123",
+  company: the_newsroom
+)
+
+user_don = User.create!(
+  email: "don@newsroom.com",
+  password: "123123",
+  company: the_newsroom
+)
+
+user_neal = User.create!(
+  email: "neal@newsroom.com",
+  password: "123123",
+  company: the_newsroom
+)
