c83637395698fd79f2d6fdf73cbe3e7e176bb8aa
george kosmopoulos <gkosmo1@hotmail.com>
2021-02-06 08:51:12 +0100

    adding the schema

--- db/schema.rb
+++ db/schema.rb
@@ -36,16 +36,6 @@ ActiveRecord::Schema.define(version: 2021_02_05_184948) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "adds", force: :cascade do |t|
-    t.string "strategy"
-    t.bigint "company_id", null: false
-    t.string "name"
-    t.string "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["company_id"], name: "index_adds_on_company_id"
-  end
-
   create_table "bad_guys", force: :cascade do |t|
     t.string "name"
     t.string "story"
@@ -179,7 +169,6 @@ ActiveRecord::Schema.define(version: 2021_02_05_184948) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "adds", "companies"
   add_foreign_key "bad_guys", "profiles"
   add_foreign_key "chosen_strategies", "profiles"
   add_foreign_key "chosen_strategies", "strategies"
