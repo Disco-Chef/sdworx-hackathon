7c470bd5d30b1e6b5c014fa0de0ce0b2fa6e3685
george kosmopoulos <gkosmo1@hotmail.com>
2021-02-06 17:44:16 +0100

    delete mentions

--- app/javascript/utils/fetch_with_token.js
+++ app/javascript/utils/fetch_with_token.js
@@ -1,7 +1,4 @@
 import { csrfToken } from "@rails/ujs";
-// https://kitt.lewagon.com/knowledge/tutorials/fetch_in_rails
-//
-//
 const fetchWithToken = (url, options) => {
   options.headers = {
     "X-CSRF-Token": csrfToken(),
