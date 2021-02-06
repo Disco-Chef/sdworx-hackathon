9ada0da7e9f4d94ca206a7b39dd78cec5e5caa9f
GuzelGokhan <gokhan@medispring.be>
2021-02-06 13:49:50 +0100

    env file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
   def home
   end
 
+  def adventure
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :profiles, except: [:show, :edit]
   resources :chosen_visions, only: [:create, :update]
   resources :chosen_strategies, only: [:create, :update]
+  resources :bad_guys, only: [:create, :update]
+  resources :quests, only: [:create, :update]
+  resources :power_ups, only: [:create, :update]
+  get 'adventure', to: "pages#adventure", as: :adventures
   get 'profile', to: "profiles#new", as: :main_profile
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
