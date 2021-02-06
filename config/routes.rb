50652f13ab08bd62789fb51f5b53c98fb574fe25
george kosmopoulos <gkosmo1@hotmail.com>
2021-02-06 13:55:47 +0100

    added profile show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'profiles/new'
   devise_for :users
   root to: 'pages#home'
-  resources :profiles, except: [:show, :edit]
+  resources :profiles, except: [:edit]
   resources :chosen_visions, only: [:create, :update]
   resources :chosen_strategies, only: [:create, :update]
   get 'profile', to: "profiles#new", as: :main_profile
